9a5bfc49dcca2bc54d27ff155e3ae0c75440be18
SaadDamine <m.sdaminesaad@gmail.com>
2020-06-07 17:15:29 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javafx-weaver-example
+Example repository for blog post about Spring Boot and JavaFX integration
+# javafx-weaver-template
---
+++ src/main/java/com/damine/javafx/TestController.java
@@ -0,0 +1,10 @@
+package com.damine.javafx;
+
+
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView("test.fxml")
+public class TestController {
+}
